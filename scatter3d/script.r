8684eb1a62a00b5e049cc73e024a719eb6e7ab13
Higor Gomes <higor.a.gomes93@gmail.com>
2021-06-05 16:18:02 -0300

    Atualização

---
+++ scatter3d/script.r
@@ -0,0 +1,21 @@
+source('./r_files/flatten_HTML.r')
+
+############### Library Declarations ###############
+libraryRequireInstall("ggplot2");
+libraryRequireInstall("plotly");
+libraryRequireInstall("dplyr");
+####################################################
+
+################### Actual code ####################
+colors <- c('#149400', '#0B88E0', '#8700E0', '#CD00E0', '#E0183B', '#E0540E', '#E0AB3C', '#E0CF19', '#C3DECC')
+p <- Values %>%
+  plot_ly( x = ~performance, y = ~potential, z = ~engagement, color = ~area, colors = colors, text = ~paste('Nome: ', nome)) %>%
+  add_markers() %>%
+  layout(scene = list(xaxis = list(title = list(text = 'Performance')),
+                      yaxis = list(title = list(text = 'Potencial')),
+                      zaxis = list(title = list(text = 'Engajamento'))), paper_bgcolor = 'white', plot_bgcolor = 'white', hoverlabel = list(bgcolor = 'white'),hoverlabel = list(bgcolor = 'white'));
+
+
+############# Create and save widget ###############
+internalSaveWidget(p, 'out.html');
+####################################################
