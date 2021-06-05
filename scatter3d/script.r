bbd7512f0e37558a276bb4a53c338cd95cf2ed48
Higor Gomes <higor.a.gomes93@gmail.com>
2021-06-05 16:32:10 -0300

    Atualização

--- scatter3d/script.r
+++ scatter3d/script.r
@@ -11,9 +11,9 @@ colors <- c('#149400', '#0B88E0', '#8700E0', '#CD00E0', '#E0183B', '#E0540E', '#
 p <- Values %>%
   plot_ly( x = ~performance, y = ~potential, z = ~engagement, color = ~area, colors = colors, text = ~paste('Nome: ', name)) %>%
   add_markers() %>%
-  layout(scene = list(xaxis = list(title = list(text = 'Performance')),
-                      yaxis = list(title = list(text = 'Potencial')),
-                      zaxis = list(title = list(text = 'Engajamento'))), paper_bgcolor = 'white', plot_bgcolor = 'white', hoverlabel = list(bgcolor = 'white'),hoverlabel = list(bgcolor = 'white'));
+  layout(scene = list(xaxis = list(title = list(text = 'Performance (x)')),
+                      yaxis = list(title = list(text = 'Potencial (y)')),
+                      zaxis = list(title = list(text = 'Engajamento (z)'))), paper_bgcolor = 'white', plot_bgcolor = 'white', hoverlabel = list(bgcolor = 'white'),hoverlabel = list(bgcolor = 'white'));
 
 
 ############# Create and save widget ###############
